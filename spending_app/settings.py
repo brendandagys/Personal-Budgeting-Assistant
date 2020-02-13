12fcfc1ef57062de64f294770e0f501abbb95e42
Brendan Dagys <brendandagys@gmail.com>
2020-02-12 22:30:30 -0500

    Commented out STATICFILES_DIRS variable

--- spending_app/settings.py
+++ spending_app/settings.py
@@ -129,8 +129,8 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 django_heroku.settings(locals())
