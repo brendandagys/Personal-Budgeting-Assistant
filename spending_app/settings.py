a922cb1ae6b298b68141877220be5164177ef410
Brendan Dagys <=>
2020-12-21 19:47:55 -0500

    Some S3 changes in settings.py. Set DISABLE_COLLECTSTATIC=1 and hoping that all files don't push to S3 for the third time.

--- spending_app/settings.py
+++ spending_app/settings.py
@@ -135,7 +135,7 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'brendanspending'
-    AWS_DEFAULT_ACL = 'public-read'
+    # AWS_DEFAULT_ACL = 'public-read'
     # AWS_QUERYSTRING_AUTH = False
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_OBJECT_PARAMETERS = {
@@ -143,6 +143,7 @@ if USE_S3:
     }
     AWS_LOCATION = 'static'
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     STATIC_URL = '/static/'
