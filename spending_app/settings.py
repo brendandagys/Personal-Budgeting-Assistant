6abe41a99f6f6ecd252a3b9cf5e9191f7ef6d44a
Brendan Dagys <=>
2020-12-23 18:52:39 -0500

    S3 works. About to find minimal settings to leave it working. Followed a few guides and a bit is still unclear.

--- spending_app/settings.py
+++ spending_app/settings.py
@@ -116,13 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Toronto'
-
 USE_I18N = False
-
 USE_L10N = False
-
 USE_TZ = False
 
 
