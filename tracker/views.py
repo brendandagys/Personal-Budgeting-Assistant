c77974e292a44e101c9df13d852cbab1d6bf12ae
Brendan Dagys <=>
2021-01-12 21:04:53 -0500

    Removed page jump, fixed AccountForm padding-right, fixed pie charts Sums mode.

--- tracker/views.py
+++ tracker/views.py
@@ -506,7 +506,10 @@ def get_pie_chart_data(request):
         elif filter_instance.pie_chart_mode == 'Sums':
             mode = 'sums'
             for category in purchase_categories_list:
-                pie_data.append((category, queryset.filter(Q(category__category=category)).aggregate(Sum('amount'))['amount__sum'] + (queryset.filter(Q(category_2__category=category, amount_2__isnull=False)).aggregate(Sum('amount_2'))['amount_2__sum'] if len(queryset.filter(Q(category_2__category=category, amount_2__isnull=False))) > 0 else 0)))
+                sum_1 = queryset.filter(Q(category__category=category, amount__isnull=False)).aggregate(Sum('amount'))['amount__sum'] if len(queryset.filter(Q(category__category=category, amount__isnull=False))) > 0 else 0
+                sum_2 = queryset.filter(Q(category_2__category=category, amount__isnull=True, amount_2__isnull=False)).aggregate(Sum('amount_2'))['amount_2__sum'] if len(queryset.filter(Q(category_2__category=category, amount__isnull=True, amount_2__isnull=False))) > 0 else 0
+                sum_3 = queryset.filter(Q(category_2__category=category, amount_2__isnull=True)).aggregate(Sum('amount'))['amount__sum'] if len(queryset.filter(Q(category_2__category=category, amount_2__isnull=True))) > 0 else 0
+                pie_data.append((category, sum_1 + sum_2))
 
         pie_data.sort(key=lambda x: x[1], reverse=True) # Reverse-sort the list of tuples by the second values: the counts
         pie_data = list(zip(*pie_data[:7])) # Only keep seven, for readability | * is unpacking operator | is a list of two tuples
