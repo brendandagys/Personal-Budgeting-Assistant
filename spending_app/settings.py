5cc9074da5bac805d2e6f1964f5e17fa1ec9a5f1
Brendan Dagys <brendandagys@gmail.com>
2021-12-28 09:52:19 -0500

    Add CORS package/middleware

--- spending_app/settings.py
+++ spending_app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker',
+    'corsheaders',
     'import_export',
     'storages',
 ]
@@ -56,11 +57,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://brendandagys.com',
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'spending_app.urls'
